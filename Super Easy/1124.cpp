6328595694673f70b2ea71e8aab50ac04c771270
kwantz <erickkwantantz123@gmail.com>
2017-08-14 11:16:38 +0700

    solve 1124

---
+++ Super Easy/1124.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+ 
+#define lli long long int
+
+int main()
+{
+    string a;
+    bool status = true;
+
+    while(getline(cin, a)) {
+		cout << a << endl;
+    }
+}
